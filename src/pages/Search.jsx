7038a5e8876ed457fd81c48b3866092b09b05ee9
Valdeci <moura97.valdeci@gmail.com>
2021-10-20 15:27:36 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import LoadingPage from './LoadingPage';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -8,10 +11,14 @@ class Search extends React.Component {
     this.state = {
       searchData: '',
       disabled: true,
+      artist: '',
+      loading: false,
+      apiReturn: '',
+      notFound: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    // this.handleSearchButton = this.handleSearchButton.bind(this);
+    this.handleSearchButton = this.handleSearchButton.bind(this);
   }
 
   handleChange({ target: { id, value } }) {
@@ -22,8 +29,39 @@ class Search extends React.Component {
     });
   }
 
+  async handleSearchButton() {
+    const { searchData } = this.state;
+    this.setState({
+      artist: searchData,
+      searchData: '',
+      loading: true,
+    });
+    const result = await searchAlbumsAPI(searchData);
+    if (result.length) {
+      this.setState({
+        loading: false,
+        apiReturn: result,
+      });
+    } else {
+      this.setState({
+        loading: false,
+        notFound: true,
+        artist: '',
+      });
+    }
+  }
+
   render() {
-    const { searchData, disabled } = this.state;
+    const { searchData, disabled, apiReturn, loading, notFound, artist } = this.state;
+
+    if (loading) {
+      return (
+        <div>
+          <Header />
+          <LoadingPage />
+        </div>
+      );
+    }
 
     return (
       <div data-testid="page-search">
@@ -42,10 +80,33 @@ class Search extends React.Component {
             type="submit"
             data-testid="search-artist-button"
             disabled={ disabled }
+            onClick={ this.handleSearchButton }
           >
             Pesquisar
           </button>
         </form>
+        <section>
+          {
+            (artist && <h2>{`Resultado de álbuns de: ${artist}`}</h2>)
+          }
+          {
+            (apiReturn) && apiReturn
+              .map(({ collectionName, artistName, artWorkUrl100, collectionId }) => (
+                <Link
+                  to={ `/album/${collectionId}` }
+                  data-testid={ `link-to-album-${collectionId}` }
+                  key={ collectionId }
+                >
+                  <img src={ artWorkUrl100 } alt="foto do album" />
+                  <span>{collectionName}</span>
+                  <span>{artistName}</span>
+                </Link>
+              ))
+          }
+          {
+            notFound && <h2>Nenhum álbum foi encontrado</h2>
+          }
+        </section>
       </div>
     );
   }
